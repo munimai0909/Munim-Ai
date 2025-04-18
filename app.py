2d3879faf7f854f6897b9b560d4389fd62dc7c5a
munimai0909 <munimai.acc@gmail.com>
2025-04-18 14:44:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from flask import Flask, render_template, request, jsonify
-import invoice_utils, ledger_utils, ocr_utils, drive_utils
-
+from logic import invoice as invoice_utils
+from logic import ledger as ledger_utils
+from logic import ocr as ocr_utils
+from logic import drive as drive_utils
 app = Flask(__name__)
 
 @app.route("/")
